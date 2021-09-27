6ab4c14727a8abf7ceb4f261b90f0c1526a7f999
bubbles linux <appenzeller.barbara@gmail.com>
2021-09-27 19:37:10 +0100

    Workshop: Node Express. finish POST

--- week-01-node/workshops/node-express-server/node-server-intro/server.js
+++ week-01-node/workshops/node-express-server/node-server-intro/server.js
@@ -3,51 +3,9 @@ const express = require('express');
 
 const server = express();
 
-//Middleware
+//Middleware for static CSS
 const staticHandler = express.static('public');
-
 server.use(staticHandler);
-
-// Add a Route: function done when server receives any request (/ is home)
-// server.get('/', (request, response) => {
-// 	response.status(200);
-// 	// set headers
-// 	response.set('funny-header', 'hihi');
-// 	response.set({
-// 		'not-funny-header': 'oh no',
-// 		'joke-header': 'haha',
-// 	});
-// 	// send message
-// 	// response.send('Hello. Great!');
-// 	const add = 4 + 1;
-// 	const time = new Date().toLocaleTimeString();
-// 	response.send(`<h1>Hello</h1><p>Great ${add} and ${time}!</p>`);
-// });
-
-// //localhost:3000/json
-// server.get('/json', (request, response) => {
-// 	response.send({ message: 'Wake up!', user: 'Bubbles' });
-// });
-
-// //localhost:3000/redirects
-// server.get('/redirects', (request, response) => {
-// 	response.redirect('/');
-// });
-
-// // dynamic paths, eg http://localhost:3000/users/Penny
-// server.get('/users/:name', (request, response) => {
-// 	const name = request.params.name;
-// 	response.send(`<h1>Hello ${name}</h1>`);
-// });
-// // server.get('/:name', (request, response) => {
-// // 	const name = request.params.name;
-// // 	response.send(`<h1>Hello ${name}</h1>`);
-// // });
-
-// // Error handling
-// server.use((request, response) => {
-// 	response.status(404).send('<h1>Disappointing!</h1>');
-// });
 
 // // Next, Middleware needs to be at top of file
 // server.get('/', (request, response, next) => {
@@ -68,6 +26,58 @@ server.use(staticHandler);
 // 	response.send('<h1>Hello</h1>');
 // });
 
+// Post: deal with form submission
+//parse form default
+const bodyParser = express.urlencoded();
+
+server.post('/submit', bodyParser, (request, response) => {
+	console.log('New user post');
+	console.log(request.body);
+	response.send('Yay, thanks for submitting');
+});
+
+// Add a Route: function done when server receives any request (/ is home)
+server.get('/', (request, response) => {
+	// set status
+	response.status(200);
+	// set headers
+	response.set('funny-header', 'hihi');
+	response.set({
+		'not-funny-header': 'oh no',
+		'joke-header': 'haha',
+	});
+	// send message
+	// response.send('Hello. Great!');
+	const add = 4 + 1;
+	const time = new Date().toLocaleTimeString();
+	response.send(`<h1>Hello</h1><p>Great ${add} and ${time}!</p>`);
+});
+
+// localhost: 3000 / json;
+server.get('/json', (request, response) => {
+	response.send({ message: 'Wake up!', user: 'Bubbles' });
+});
+
+//localhost:3000/redirects
+server.get('/redirects', (request, response) => {
+	response.redirect('/');
+});
+
+// dynamic paths, eg http://localhost:3000/users/Penny
+server.get('/users/:name', (request, response) => {
+	const name = request.params.name;
+	response.send(`<h1>Hello ${name}</h1>`);
+});
+// server.get('/:name', (request, response) => {
+// 	const name = request.params.name;
+// 	response.send(`<h1>Hello ${name}</h1>`);
+// });
+
+// Error handling
+server.use((request, response) => {
+	response.status(404).send('<h1>Disappointing!</h1>');
+});
+
 // Start the Server: connect to internet, have port listen to incoming HTTP request
 const PORT = 3000;
 
@@ -76,15 +86,3 @@ server.listen(PORT, () =>
 );
 // Terminal: $ node server.js --> now can see http://localhost:3000 in Browser --> hello
 // Stop Node --> Terminal: Strg C
-
-// Send requests: 3000
-
-// Response: response.status(404)
-
-// Some static files in public folder --> CSS, HTML, img
-
-// Post: deal with form submission
-server.post('/submit', (request, response) => {
-	console.log('posted');
-	response.send('Yay, thanks for submitting');
-});
