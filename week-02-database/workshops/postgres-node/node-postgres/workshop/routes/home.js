087a2389eb5d1b95d33e653a84b738672e6bfa1d
bubbles linux <appenzeller.barbara@gmail.com>
2021-10-04 14:35:40 +0100

    Workshop: Node Postgres. Install npm

--- week-02-database/workshops/postgres-node/node-postgres/workshop/routes/home.js
+++ week-02-database/workshops/postgres-node/node-postgres/workshop/routes/home.js
@@ -1,5 +1,15 @@
+// Import database connection
+const db = require('../database/connection.js');
+
 function get(request, response) {
-  response.send(`<h1>Hello world</h1>`);
+	// send SQL command to db. Promise waits for its return
+	db.query('SELECT * FROM users').then(result => {
+		// console.log(result);
+		const users = result.rows;
+		const userList = users.map(user => `<li>${user.username}</li>`).join('');
+		response.send(`<ul>${userList}</ul>`);
+	});
+	// response.send(`<h1>Hello world</h1>`);
 }
 
 module.exports = { get };
