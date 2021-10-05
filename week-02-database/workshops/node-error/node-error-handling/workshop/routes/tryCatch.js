f6bf203be4b65c4a054352c95d7a64f13f6ebaff
bubbles linux <appenzeller.barbara@gmail.com>
2021-10-05 15:42:49 +0100

    Workshop node error. packages

--- week-02-database/workshops/node-error/node-error-handling/workshop/routes/tryCatch.js
+++ week-02-database/workshops/node-error/node-error-handling/workshop/routes/tryCatch.js
@@ -1,8 +1,13 @@
-const layout = require("../layout.js");
+const layout = require('../layout.js');
 
 function get(request, response) {
-  const html = layot(`<h1>Gonna error</h1>`);
-  response.send(html);
+	try {
+		const html = layot(`<h1>Gonna error</h1>`);
+		response.send(html);
+	} catch (error) {
+		console.error(error);
+		response.status(500).send(`<h1>Server error blubblubbb</h1>`);
+	}
 }
 
 module.exports = { get };
--- week-02-database/workshops/node-error/node-error-handling/workshop/server.js
+++ week-02-database/workshops/node-error/node-error-handling/workshop/server.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 
-const home = require("./routes/home.js");
-const tryCatch = require("./routes/tryCatch.js");
-const rejection = require("./routes/rejection.js");
+const home = require('./routes/home.js');
+const tryCatch = require('./routes/tryCatch.js');
+const rejection = require('./routes/rejection.js');
 
 const server = express();
 
-server.get("/", home.get);
-server.get("/try-catch", tryCatch.get);
-server.get("/rejection", rejection.get);
+server.get('/', home.get);
+server.get('/try-catch', tryCatch.get);
+server.get('/rejection', rejection.get);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
