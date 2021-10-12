f5621e5090829108ba954269f43a817589f327f8
bubbles linux <appenzeller.barbara@gmail.com>
2021-10-12 11:57:46 +0100

    W: session auth. set up. task1

---
+++ week-03-auth/workshops/session/session-auth/workshop/database/model.js
@@ -0,0 +1,45 @@
+const db = require('./connection.js');
+
+function createUser(email, hash, name) {
+	const INSERT_USER = `
+    INSERT INTO users (email, password, name) VALUES ($1, $2, $3)
+    RETURNING id, email, name
+  `;
+	return db
+		.query(INSERT_USER, [email, hash, name])
+		.then(result => result.rows[0]);
+}
+
+function createSession(sid, datObj) {
+	const INSERT_SESSION = /*sql*/ `
+    INSERT INTO sessions
+    (sid, data)
+    VALUES ($1, $2) 
+    RETURNING sid
+  `;
+	return db
+		.query(INSERT_SESSION, [sid, datObj])
+		.then(result => console.log(result.rows[0].sid));
+}
+
+function getUser(email) {
+	const SELECT_USER = `
+  SELECT id, email, password, name FROM users WHERE email=$1
+  `;
+	return db.query(SELECT_USER, [email]).then(result => result.rows[0]);
+}
+
+function getSession(sid) {
+	const SELECT_SESSION = 'SELECT data FROM sessions WHERE sid=$1';
+	return db.query(SELECT_SESSION, [sid]).then(result => {
+		const singleResult = result.rows[0];
+		return singleResult && singleResult.data;
+	});
+}
+
+module.exports = {
+	createUser,
+	getUser,
+	createSession,
+	getSession,
+};
---
+++ week-03-auth/workshops/session/session-auth/workshop/routes/logIn.js
@@ -0,0 +1,26 @@
+const auth = require('../auth.js');
+const model = require('../database/model.js');
+
+function get(request, response) {
+	response.send(`
+    <h1>Log in</h1>
+    <form action="log-in" method="POST">
+      <label for="email">Email</label>
+      <input type="email" id="email" name="email">
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password">
+      <button>Log in</button>
+    </form>
+  `);
+}
+
+function post(request, response) {
+	const { email, password } = request.body;
+	model
+		.createSession('def456', { just: 'testing things' })
+		.then(sid => console.log(`Logging in ${sid}`))
+		.then(() => response.redirect('/'));
+	// Logs: "def456"
+}
+
+module.exports = { get, post };
