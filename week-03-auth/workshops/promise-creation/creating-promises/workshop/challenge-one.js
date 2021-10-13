4886e8af19e0fa6cfce004683360c7db9eaba198
bubbles linux <appenzeller.barbara@gmail.com>
2021-10-13 10:45:19 +0100

    W: promisify. finish

--- week-03-auth/workshops/promise-creation/creating-promises/workshop/challenge-one.js
+++ week-03-auth/workshops/promise-creation/creating-promises/workshop/challenge-one.js
@@ -7,9 +7,15 @@
  */
 
 function wait(ms) {
-  //
+	//
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, ms);
+	});
 }
 
+function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
 /*
  *
  *
@@ -34,22 +40,22 @@ function wait(ms) {
  *
  *
  */
-const test = require("tape");
+const test = require('tape');
 
-test("`wait` function works correctly", (t) => {
-  const start = Date.now();
-  const result = wait(200);
-  t.true(result instanceof Promise, "wait should return a promise");
-  if (result) {
-    return result.then(() => {
-      const end = Date.now();
-      const duration = end - start;
-      // can't be too specific here as setTimeout is never exact
-      t.true(duration > 199, "`wait(200)` should wait at least 200ms");
-      t.true(duration < 250, "`wait(200)` shouldn't wait longer than 200ms");
-      t.end();
-    });
-  } else {
-    t.end();
-  }
+test('`wait` function works correctly', t => {
+	const start = Date.now();
+	const result = wait(200);
+	t.true(result instanceof Promise, 'wait should return a promise');
+	if (result) {
+		return result.then(() => {
+			const end = Date.now();
+			const duration = end - start;
+			// can't be too specific here as setTimeout is never exact
+			t.true(duration > 199, '`wait(200)` should wait at least 200ms');
+			t.true(duration < 250, "`wait(200)` shouldn't wait longer than 200ms");
+			t.end();
+		});
+	} else {
+		t.end();
+	}
 });
--- week-03-auth/workshops/promise-creation/creating-promises/workshop/challenge-two.js
+++ week-03-auth/workshops/promise-creation/creating-promises/workshop/challenge-two.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 /*
  *
@@ -10,9 +10,17 @@ const path = require("path");
  */
 
 function readFilePromise(filePath) {
-  //
+	//
+	return new Promise((resolve, reject) => {
+		fs.readFile(filePath, (err, data) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(data);
+			}
+		});
+	});
 }
-
 /*
  *
  *
@@ -35,50 +43,50 @@ function readFilePromise(filePath) {
  *
  *
  */
-const test = require("tape");
+const test = require('tape');
 
-test("readFilePromise returns a promise", (t) => {
-  const testPath = path.join(__dirname, "test.txt");
-  const result = readFilePromise(testPath);
-  t.true(
-    result instanceof Promise,
-    "readFilePromise should return a Promise object"
-  );
-  if (result) {
-    result.then((contents) => {
-      t.equal(
-        contents.toString(),
-        "hello",
-        `readFilePromise("./test/txt") should be should be 'hello', received '${contents}'`
-      );
-      t.end();
-    });
-  } else {
-    t.end();
-  }
+test('readFilePromise returns a promise', t => {
+	const testPath = path.join(__dirname, 'test.txt');
+	const result = readFilePromise(testPath);
+	t.true(
+		result instanceof Promise,
+		'readFilePromise should return a Promise object'
+	);
+	if (result) {
+		result.then(contents => {
+			t.equal(
+				contents.toString(),
+				'hello',
+				`readFilePromise("./test/txt") should be should be 'hello', received '${contents}'`
+			);
+			t.end();
+		});
+	} else {
+		t.end();
+	}
 });
 
-test("readFilePromise rejects if an error occurs", (t) => {
-  const result = readFilePromise("notReal.psd");
-  if (result) {
-    result
-      .then(() => {
-        t.fail("Nonexistent file should cause readFilePromise to reject");
-        t.end();
-      })
-      .catch((error) => {
-        t.ok(
-          error instanceof Error,
-          "readFilePromise should reject with an Error object"
-        );
-        t.equal(
-          error.message,
-          "ENOENT: no such file or directory, open 'notReal.psd'",
-          `Error message should be "ENOENT: no such file or directory, open 'notReal.psd'", received "${error.message}"`
-        );
-        t.end();
-      });
-  } else {
-    t.end();
-  }
+test('readFilePromise rejects if an error occurs', t => {
+	const result = readFilePromise('notReal.psd');
+	if (result) {
+		result
+			.then(() => {
+				t.fail('Nonexistent file should cause readFilePromise to reject');
+				t.end();
+			})
+			.catch(error => {
+				t.ok(
+					error instanceof Error,
+					'readFilePromise should reject with an Error object'
+				);
+				t.equal(
+					error.message,
+					"ENOENT: no such file or directory, open 'notReal.psd'",
+					`Error message should be "ENOENT: no such file or directory, open 'notReal.psd'", received "${error.message}"`
+				);
+				t.end();
+			});
+	} else {
+		t.end();
+	}
 });
