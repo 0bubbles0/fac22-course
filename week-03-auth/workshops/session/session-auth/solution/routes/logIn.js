cf0598daf0eccc76df0c006b6ae7b5eeb65aaafa
bubbles linux <appenzeller.barbara@gmail.com>
2021-10-13 16:56:40 +0100

    typos

--- week-03-auth/workshops/session/session-auth/solution/routes/logIn.js
+++ week-03-auth/workshops/session/session-auth/solution/routes/logIn.js
@@ -1,7 +1,7 @@
-const auth = require("../auth.js");
+const auth = require('../auth.js');
 
 function get(request, response) {
-  response.send(`
+	response.send(`
     <h1>Log in</h1>
     <form action="log-in" method="POST">
       <label for="email">Email</label>
@@ -14,18 +14,18 @@ function get(request, response) {
 }
 
 function post(request, response) {
-  const { email, password } = request.body;
-  auth
-    .verifyUser(email, password)
-    .then(auth.saveUserSession)
-    .then((sid) => {
-      response.cookie("sid", sid, auth.COOKIE_OPTIONS);
-      response.redirect("/");
-    })
-    .catch((error) => {
-      console.error(error);
-      response.send(`<h1>User not found</h1>`);
-    });
+	const { email, password } = request.body;
+	auth
+		.verifyUser(email, password)
+		.then(auth.saveUserSession)
+		.then(sid => {
+			response.cookie('sid', sid, auth.COOKIE_OPTIONS);
+			response.redirect('/');
+		})
+		.catch(error => {
+			console.error(error);
+			response.send(`<h1>User not found</h1>`);
+		});
 }
 
 module.exports = { get, post };
